bb78ea6d611fbbe6745d180d231a7f367d99c190
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-12-27 11:27:49 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="app">
+            <h1>Vocabulary SRS</h1>
+            <p>Frontend for backend demo</p>
+        </div>
+    );
+}
+
+export default App;
+
