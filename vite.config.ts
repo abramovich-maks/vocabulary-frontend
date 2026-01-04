632123cd105623b775cb8ab7b87faa3e6fb8aae3
abramovich-maks <maks.abramovich.1995@mail.ru>
2026-01-04 21:30:38 +0100

    Add Dockerfile for production build and comment out development server proxy in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,16 @@ import { defineConfig } from 'vite';
 export default () => {
   return defineConfig({
     plugins: [react()],
-    server: {
-      port: 3000,
-      proxy: {
-        '/api': {
-          target: 'http://localhost:8080',
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
-      },
-    },
+    // server: {
+    //   port: 3000,
+    //   proxy: {
+    //     '/api': {
+    //       target: 'http://localhost:8080',
+    //       changeOrigin: true,
+    //       secure: false,
+    //       rewrite: (path) => path.replace(/^\/api/, ''),
+    //     },
+    //   },
+    // },
   });
 };
